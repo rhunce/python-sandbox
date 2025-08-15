6f62f37f7d3ec255cd5600dbec1f895a118b52a1
Raymond Hunce <hunce18c@gmail.com>
2025-08-15 10:24:57 -0400

    Set up problem skeleton

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
-def main():
-    print("Hello, Python project!")
+"""
 
-if __name__ == "__main__":
-    main()
+Inputs:
+- lyrics: string
+- band_name: string
+
+Output:
+- The lyrics arranged in such a way that the band name is spelled vertically therein: string
+- Remove all non-alphanumeric characters
+
+Example:
+lyrics = "...I bomb atomically, socrates, philosophies and hypotheses can't define how I be dropping these mockeries..."
+band_name = "miee"
+
+output =    "I bomb atomically socrates 
+               philosophies and hypotheses 
+       cant define how I be dropping 
+             these mockeries"
+                 ^
+"""
+
+def arrange_lyrics(lyrics, band_name):
+  return 5
+
+lyrics = "...I bomb atomically, socrates, philosophies and hypotheses can't define how I be dropping these mockeries..."
+band_name = "miee"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 import unittest
-from src.main import main
+from src.main import arrange_lyrics, band_name
 
-class TestMain(unittest.TestCase):
+class TestArrangeLyrics(unittest.TestCase):
     def test_main_runs(self):
-        self.assertIsNone(main())
+        lyrics = "...I bomb atomically, socrates, philosophies and hypotheses can't define how I be dropping these mockeries..."
+        band_name = "miee"
+        self.assertEqual(arrange_lyrics(lyrics, band_name), 5)
 
 if __name__ == "__main__":
     unittest.main()
